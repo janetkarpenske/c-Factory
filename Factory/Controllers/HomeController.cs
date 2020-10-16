6d9987fd04558bc883aae43bc62303b46c067d01
Janet Karpenske <janetkarpenske@gmail.com>
2020-10-16 13:40:18 -0700

    Add styling to homepage as well as header with home button

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,9 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+// private readonly FactoryContext _db;
+
+//     public HomeController(FactoryContext db)
+//     {
+//       _db = db;
+//     }
+
+
+
     [HttpGet("/")]
     public ActionResult Index()
     {
